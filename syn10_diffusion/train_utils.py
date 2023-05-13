4f0809569caa4fdd0181e607fe47b1ea900b9a06
obaghirli <orkhan31415@gmail.com>
2023-05-13 10:52:25 +0400

    feat: lr_scheduler, bugfix: optimizer model parameters inited after ddp state sync

--- syn10_diffusion/train_utils.py
+++ syn10_diffusion/train_utils.py
@@ -17,6 +17,7 @@ class Trainer:
         self.model = model
         self.diffusion = diffusion
         self.data = data
+        self.iters = len(data)
         self.rank = int(os.environ['RANK'])
         self.local_rank = int(os.environ['LOCAL_RANK'])
         self.world_size = int(os.environ['WORLD_SIZE'])
@@ -28,29 +29,66 @@ class Trainer:
         self.checkpoint_freq = params['checkpoint_freq']
         self.tensorboard_freq = params['tensorboard_freq']
         self.step = 0
+        self.local_step = 0
         self.start_epoch = 0
-        self.resume_step = None
 
         self.dlogger = DistributedLogger()
         self.tb_writer = TBSummaryWriter(log_dir=self.run_dir)
 
+        self.ddp_model = DDP(self.model, device_ids=[self.local_rank], output_device=self.local_rank)
         if params['resume_step'] is not None:
             self.load_model_checkpoint(params['resume_step'])
 
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=params['lr'])
-
+        self.optimizer = torch.optim.AdamW(
+            self.ddp_model.module.parameters(),
+            lr=params['lr'],
+            weight_decay=params['weight_decay']
+        )
         if params['resume_step'] is not None:
             self.load_optimizer_checkpoint(params['resume_step'])
 
-        self.ddp_model = DDP(self.model, device_ids=[self.local_rank], output_device=self.local_rank)
-        self.ema = EMA(self.ddp_model, decay=params['ema_decay'], delay=params['ema_delay'])
+        self.lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
+            self.optimizer,
+            T_0=params['lr_scheduler_t_0'],
+            T_mult=params['lr_scheduler_t_mult']
+        )
+        if params['resume_step'] is not None:
+            self.load_lr_scheduler_checkpoint(params['resume_step'])
 
+        self.ema = EMA(self.ddp_model, decay=params['ema_decay'], delay=params['ema_delay'])
         if params['resume_step'] is not None and self.step >= self.ema.delay:
             self.load_ema_checkpoint(params['resume_step'])
 
         if params['resume_step'] is not None:
-            self.resume_step = params['resume_step']
+            self.local_step += 1
+            if self.local_step >= self.iters:
+                self.local_step = 0
+                self.start_epoch += 1
             self.step += 1
+
+    def load_model_checkpoint(self, resume_step):
+        model_checkpoint_path = self.run_dir / f"model_checkpoint_{resume_step}.pt.tar"
+        if not model_checkpoint_path.exists():
+            raise FileNotFoundError(f"Model checkpoint {model_checkpoint_path} does not exist")
+        model_checkpoint = torch.load(model_checkpoint_path, map_location=f"cuda:{self.local_rank}")
+        self.ddp_model.module.load_state_dict(model_checkpoint['model_state_dict'])
+
+    def load_optimizer_checkpoint(self, resume_step):
+        optimizer_checkpoint_path = self.run_dir / f"optimizer_checkpoint_{resume_step}.pt.tar"
+        if not optimizer_checkpoint_path.exists():
+            raise FileNotFoundError(f"Optimizer checkpoint {optimizer_checkpoint_path} does not exist")
+        optimizer_checkpoint = torch.load(optimizer_checkpoint_path, map_location=f"cuda:{self.local_rank}")
+        self.optimizer.load_state_dict(optimizer_checkpoint['optimizer_state_dict'])
+
+    def load_lr_scheduler_checkpoint(self, resume_step):
+        lr_scheduler_checkpoint_path = self.run_dir / f"lr_scheduler_checkpoint_{resume_step}.pt.tar"
+        if not lr_scheduler_checkpoint_path.exists():
+            raise FileNotFoundError(f"LR scheduler checkpoint {lr_scheduler_checkpoint_path} does not exist")
+        lr_scheduler_checkpoint = torch.load(lr_scheduler_checkpoint_path, map_location=f"cuda:{self.local_rank}")
+        self.lr_scheduler.load_state_dict(lr_scheduler_checkpoint['lr_scheduler_state_dict'])
+        self.local_step = lr_scheduler_checkpoint['local_step']
+        self.step = lr_scheduler_checkpoint['step']
+        self.start_epoch = lr_scheduler_checkpoint['epoch']
 
     def load_ema_checkpoint(self, resume_step):
         ema_chckpoint_path = self.run_dir / f"ema_checkpoint_{resume_step}.pt.tar"
@@ -59,25 +97,10 @@ class Trainer:
         ema_checkpoint = torch.load(ema_chckpoint_path, map_location=f"cuda:{self.local_rank}")
         self.ema.load_state_dict(ema_checkpoint['model_state_dict'])
 
-    def load_model_checkpoint(self, resume_step):
-        model_checkpoint_path = self.run_dir / f"model_checkpoint_{resume_step}.pt.tar"
-        if not model_checkpoint_path.exists():
-            raise FileNotFoundError(f"Model checkpoint {model_checkpoint_path} does not exist")
-        model_checkpoint = torch.load(model_checkpoint_path, map_location=f"cuda:{self.local_rank}")
-        self.model.load_state_dict(model_checkpoint['model_state_dict'])
-
-    def load_optimizer_checkpoint(self, resume_step):
-        optimizer_checkpoint_path = self.run_dir / f"optimizer_checkpoint_{resume_step}.pt.tar"
-        if not optimizer_checkpoint_path.exists():
-            raise FileNotFoundError(f"Optimizer checkpoint {optimizer_checkpoint_path} does not exist")
-        optimizer_checkpoint = torch.load(optimizer_checkpoint_path, map_location=f"cuda:{self.local_rank}")
-        self.optimizer.load_state_dict(optimizer_checkpoint['optimizer_state_dict'])
-        self.step = optimizer_checkpoint['step']
-        self.start_epoch = optimizer_checkpoint['epoch']
-
-    def save_checkpoint(self, step, epoch):
+    def save_checkpoint(self, local_step, step, epoch):
         model_checkpoint_path = self.run_dir / f"model_checkpoint_{step}.pt.tar"
         optimizer_checkpoint_path = self.run_dir / f"optimizer_checkpoint_{step}.pt.tar"
+        lr_scheduler_checkpoint_path = self.run_dir / f"lr_scheduler_checkpoint_{step}.pt.tar"
         ema_checkpoint_path = self.run_dir / f"ema_checkpoint_{step}.pt.tar"
 
         torch.save({
@@ -85,10 +108,15 @@ class Trainer:
         }, model_checkpoint_path)
 
         torch.save({
-            'step': step,
-            'epoch': epoch,
             'optimizer_state_dict': self.optimizer.state_dict()
         }, optimizer_checkpoint_path)
+
+        torch.save({
+            'local_step': local_step,
+            'step': step,
+            'epoch': epoch,
+            'lr_scheduler_state_dict': self.lr_scheduler.state_dict()
+        }, lr_scheduler_checkpoint_path)
 
         if step >= self.ema.delay:
             torch.save({
@@ -96,13 +124,22 @@ class Trainer:
             }, ema_checkpoint_path)
 
     def run(self):
-        avg_loss = torch.zeros(1).to(self.local_rank)
-        avg_mse_loss = torch.zeros(1).to(self.local_rank)
-        avg_vlb_loss = torch.zeros(1).to(self.local_rank)
+        avg_loss = torch.FloatTensor([0.]).to(self.local_rank)
+        avg_mse_loss = torch.FloatTensor([0.]).to(self.local_rank)
+        avg_vlb_loss = torch.FloatTensor([0.]).to(self.local_rank)
 
         for epoch in range(self.start_epoch, self.num_epochs):
-            for i, (x, y) in enumerate(self.data):
-                print(f"Rank: {self.rank}, batch size: {x.shape[0]}, epoch: {epoch}, step: {self.step}", end='\r')
+            for i, (x, y) in enumerate(self.data, self.local_step):
+                if i == self.iters:
+                    break
+
+                print(f"Rank: {self.rank}, "
+                      f"batch size: {x.shape[0]}, "
+                      f"i_batch: {i + 1}/{self.iters}, "
+                      f"epoch: {epoch}, "
+                      f"local_step: {i}, "
+                      f"step: {self.step}")
+
                 if self.step == self.ema.delay:
                     self.ema.build_shadow()
 
@@ -119,23 +156,24 @@ class Trainer:
                 mse = loss_terms['mse'].mean()
                 vlb = loss_terms['vlb'].mean()
 
-                avg_loss += loss.clone() / self.tensorboard_freq
-                avg_mse_loss += mse.clone() / self.tensorboard_freq
-                avg_vlb_loss += vlb.clone() / self.tensorboard_freq
+                avg_loss += loss.detach().clone() / self.tensorboard_freq
+                avg_mse_loss += mse.detach().clone() / self.tensorboard_freq
+                avg_vlb_loss += vlb.detach().clone() / self.tensorboard_freq
 
                 self.optimizer.zero_grad()
                 loss.backward()
 
                 try:
-                    clip_grad_norm_(self.ddp_model.parameters(), self.grad_clip)
+                    clip_grad_norm_(self.ddp_model.module.parameters(), self.grad_clip)
                 except Exception:
                     pass
 
                 self.optimizer.step()
+                self.lr_scheduler.step(epoch + i / self.iters)
                 self.ema.step()
 
                 if self.step % self.checkpoint_freq == 0 and self.rank == 0 and self.step > 0:
-                    self.save_checkpoint(self.step, epoch)
+                    self.save_checkpoint(i, self.step, epoch)
                 if self.step % self.tensorboard_freq == 0 and self.rank == 0 and self.step > 0:
                     dist.all_reduce(avg_loss, op=dist.ReduceOp.SUM)
                     dist.all_reduce(avg_mse_loss, op=dist.ReduceOp.SUM)
@@ -147,11 +185,13 @@ class Trainer:
                         "train", {
                             "avg_loss": avg_loss.item(),
                             "avg_mse_loss": avg_mse_loss.item(),
-                            "avg_vlb_loss": avg_vlb_loss.item()
+                            "avg_vlb_loss": avg_vlb_loss.item(),
+                            "lr": self.optimizer.param_groups[0]['lr']
                         }, self.step
                     )
                     avg_loss.zero_()
                     avg_mse_loss.zero_()
                     avg_vlb_loss.zero_()
-                self.step += 1
                 dist.barrier()
+                self.step += 1
+            self.local_step = 0
--- syn10_diffusion/utils.py
+++ syn10_diffusion/utils.py
@@ -12,18 +12,24 @@ def seed_all(seed=313):
         seed (int): Seed value for random number generation.
     """
     try:
-        import torch
-        torch.manual_seed(seed)
-        if torch.cuda.is_available():
-            torch.cuda.manual_seed_all(seed)
-            torch.backends.cudnn.deterministic = True
-            torch.backends.cudnn.benchmark = False
+        import random
+        random.seed(seed)
     except ImportError:
         pass
 
     try:
         import numpy as np
         np.random.seed(seed)
+    except ImportError:
+        pass
+
+    try:
+        import torch
+        torch.manual_seed(seed)
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed_all(seed)
+            torch.backends.cudnn.deterministic = True
+            torch.backends.cudnn.benchmark = False
     except ImportError:
         pass
 
@@ -69,6 +75,8 @@ def validate_config(config: dict):
     assert config["model_channels"] > 0
     assert config["num_resnet_blocks"] > 0
     assert config["lr"] > 0.0
+    assert config["lr_scheduler_t_0"] > 0
+    assert config["lr_scheduler_t_mult"] > 0
     assert config["grad_clip"] > 0.0
     assert config["train_batch_size"] > 0
     assert config["checkpoint_freq"] > 0
@@ -77,6 +85,7 @@ def validate_config(config: dict):
     assert config["guidance"] > 0.0
     assert 0.0 < config["ema_decay"] < 1.0
     assert config["ema_delay"] >= 0
+    assert config["weight_decay"] >= 0.0
     assert config["image_max_value"] > config["image_min_value"] >= 0
     assert config["num_classes"] > 1
     assert 1.0 > config["max_beta"] > 0.0
